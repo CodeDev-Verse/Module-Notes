4505855bde415bebdf0ccc6fad00ce028af35fa5
rishabh32700 <rishabhpep@gmail.com>
2025-03-22 20:56:02 +0530

    day 5 notes

---
+++ Module 1 JS/Module 1 Day 5/index.js
@@ -0,0 +1,91 @@
+// // let a = 100
+// // console.log(a);
+
+// // function b(prams) {
+// //     var a = 10
+// //     console.log(a);
+// // }
+
+// // {
+// //     var a = 10
+// //     console.log(a);
+// // }
+
+// // const obj = {
+// //     key:"val"
+// // }
+
+// let a = 10;
+// let b = a; 
+// // a is replaced by its value which is 10
+// console.log(b);
+
+// // b = 20;
+// // console.log(a); // 10 (primitive data types are copied by value)
+
+// let obj1 = { name: "Alice" };
+// console.log(obj1.name);
+
+// let obj2 = obj1;
+// // ideally it should do like this 
+
+// // obj2 = { name: "Alice" }
+// // and then when i am logging ojb2.name it should print alice
+// obj2.name = "Bob";
+// console.log(obj1.name); // "Bob" (objects are copied by reference)
+// var a = 10
+// a += 5
+// // a = a +5
+// a -= 5
+// // a = a- 5
+// console.log(a);
+
+// a++
+// // a = a+1
+// console.log(a);
+// console.log(typeof(a));
+// var b ="hello"
+// console.log(typeof(b));
+
+// let b = (1,2,3,4,5)
+// let b = 1
+// b = 2
+// b = 3
+// b = 4
+// b = 5
+
+
+// let age = 19;
+// age >= 18 ? console.log("adult") : console.log("minor");
+
+
+// if(age >= 18){
+//     console.log("adult");
+// }else{
+//     console.log("minor");
+// }
+
+// var x = 10
+// var z = x++
+// var y = ++x
+
+// console.log(x,y,z);
+
+// console.log(10%3);
+// console.log(10%5);
+// console.log(2**3);
+// console.log(2**2);
+// console.log("5" + 2);
+// console.log("5" + "2");
+// console.log(5 + "2");
+// console.log("5" - 2);
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+// console.log(1 === 1);
+// console.log("hello" * 2);
+console.log(0.1 + 0.2 == 0.3);
+console.log(0.1 + 0.2 );
+console.log(0.1 + 0.2 == 0.30000000000000004);
+console.log((0.1 + 0.2).toFixed(17) == 0.3);
+
+

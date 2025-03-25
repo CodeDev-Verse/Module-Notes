8131b890ae0ebf6743dff6c34d99f5549c8de22c
rishabh32700 <rishabhpep@gmail.com>
2025-03-25 22:43:02 +0530

    inclass noteds day 7

---
+++ Module 1 JS/Module 1 Day 7/index.js
@@ -0,0 +1,101 @@
+// var age = 17
+
+// if(age<=18) console.log("minor");
+// else console.log("adult");
+
+// if (age>=18){
+//     console.log("Hello");
+//     console.log("World");
+// }
+
+// if ("0") {
+//     console.log("This runs!"); // "0" is truthy
+// }
+// if (0) {
+//     console.log("This runs 2!"); // 0 is falsy
+// }
+
+// if(null == undefined){
+//     console.log("hello"); 
+// }
+
+// if(null === undefined){
+//     console.log("bye"); 
+// }
+
+// if("0"){
+//     console.log("inside if");
+// }
+
+// if ("0" === false) {  
+//     console.log("hello");
+// }else{
+//     console.log("bye");
+// }
+
+
+// if (user) {
+//     if (user.isActive) {
+//         if (user.role === "admin") {
+//             console.log("Access granted!");
+//         }
+//     }
+// }
+
+
+// if(user || user.isActive || user.role === "admin"){
+//     console.log("Access granted!");
+// }
+
+// if(!user || !user.isActive || user.role !== "admin") return
+// pyramid of doom
+
+
+
+// let fruit = "apple";
+// switch (fruit) {
+//     case "apple":
+//         console.log("It's an apple 1!");
+        
+//     case "apple":
+//         console.log("It's an apple 2!");
+//         break;
+//     case "mango":
+//         console.log("It's a banana!");
+//         break;
+//     default:
+//         console.log("Unknown fruit");
+// }
+
+// if(fruit == "apple"){
+
+// }else if(fruit == "banana"){
+
+// }else if(fruit == "mang"){
+
+// }else{
+
+// }
+// let value = "10";
+// switch (value) {
+//     case 10:
+//         console.log("Matched!"); // Won't match due to strict comparison
+//         break;
+// }
+
+// var age = 19
+
+// if(age>=18){
+//     let a = 10
+//     console.log(a);
+    
+// }else{
+//     let b = 20
+//     console.log(b);
+    
+// }
+
+let result = true ? false : true ? false : true;
+console.log(result);
+
+

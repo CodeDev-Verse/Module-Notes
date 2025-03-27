e83ce06f417238bd6551dbd59d57b24016e59cf2
rishabh32700 <rishabhpep@gmail.com>
2025-03-27 22:40:35 +0530

    day 8 notes pushed

---
+++ Module 1 JS/Module 1 Day 8/index.js
@@ -0,0 +1,32 @@
+
+for (var i = 0; i < 10; i++) {
+    if(i === 5) continue;
+    console.log(i);
+}
+
+// let count = 0
+
+// while (count < 5){
+//      console.log(count);
+//     count++
+//     }
+
+    // do while
+
+    // let input;
+    // do{
+    //     input = prompt("ennter a NUMBER GREATER TAAN 10")
+    // }while(input <= 10) console.log(input);
+    
+
+    // for in
+    // const obj = {a: 1, b:2, c:3, d:4}
+    // for (let i in obj){
+    //     console.log(i, obj[i]);
+        
+    // }
+
+
+    // const arr = [1,2,3,4,5,6]
+    // for (let a of arr) console.log(a);
+    

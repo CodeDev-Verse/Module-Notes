0ac9e3154eca5ad274c7617837445d89bb976418
rishabh32700 <rishabhpep@gmail.com>
2025-03-28 22:45:17 +0530

    day 11 notes

---
+++ Module 1 JS/Module 1 Day 9/index.js
@@ -0,0 +1,38 @@
+// // func dec
+// function test() {
+//     console.log("test");
+// }
+// test()
+// // named func expression
+// var b = function hello() {
+//     console.log("b");
+// }
+// b()
+// // hello()
+// // ann func expression
+// var c = function () {
+//     console.log("c");
+// }
+// c()
+// arrow funct
+// var d = () =>{
+//     console.log(arguments);
+// }
+// d(1,2,3,4,5,6)
+// function test() {
+//     console.log(arguments);
+// }
+// test(1,2,3,4,5,6)
+// this keyword
+// IIFE
+(() =>{
+    console.log("some funct");
+})()
+
+
+// function greet( ...rest) {
+//     console.log(rest);
+//     console.log(arguments);
+// }
+// greet("rishabh", "hello", "bye", 1,2,3,4,5);
+
